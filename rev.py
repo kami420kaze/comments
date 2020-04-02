32167cb0338c87303435cff931caf16cab58195d
Terry Ouyang <yamerocat520@gmail.com>
2020-04-02 17:48:29 +0800

    show rev

--- rev.py
+++ rev.py
@@ -1,12 +1,12 @@
+import random
 data = []
 num = 0
 i = 1
-
 with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		num += 1
-		if num == 1000 * i: #老師的寫法 num % 1000 == 0
+		if num == 10000 * i: #老師的寫法 num % 1000 == 0
 			print(len(data)) # % 是求「餘數」符號 ex 7 % 3 = 1
 			i += 1
 
@@ -24,4 +24,13 @@ for d in data:
 	if len(d) < 100:
 		new.append(d.strip())
 print('總共有', len(new), '筆資料小於100字')
-print(new)
+
+shit = []
+for d in data:
+	if 'shit' in d:
+		shit.append(d.strip())
+x = (len(shit))
+r = random.randint(0, x)
+print('一共有', len(shit), '人覺得這個產品很shit')
+print('有人說:', shit[r])
+print('這是其中的第', r, '筆')
