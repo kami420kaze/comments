9e7dfb02b1654f813770303711a7ae0b4641aab9
Terry Ouyang <yamerocat520@gmail.com>
2020-04-02 13:31:11 +0800

    second

---
+++ rev.py
@@ -0,0 +1,13 @@
+data = []
+num = 0
+i = 1
+with open ('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line.strip())
+		num += 1
+		if num == 1000 * i: #老師的寫法 num % 1000 == 0
+			print(len(data)) # % 是求「餘數」符號 ex 7 % 3 = 1
+			i += 1
+print(data[0])
+print('---------------------')
+print(data[1])
